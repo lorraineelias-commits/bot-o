5c35de5d7f60b12ae37880fbca89347287339272
lorraineelias-commits <lorraine.elias@escola.pr.gov.br>
2026-06-26 10:51:34 +0000

    oi

---
+++ script.js
@@ -0,0 +1,9 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+     })
+})
+let botao = document.querySelector('.meu-botao');
+let paragrafos = document.querySelectorAll('p');
